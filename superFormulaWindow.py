19fd822af91981c6c54581dadb5e47eef1a02894
bodomus <bodomus@gmail.com>
2016-03-07 17:02:38 +0200

    main window

--- superFormulaWindow.py
+++ superFormulaWindow.py
@@ -3,14 +3,21 @@ from PySide.QtCore import *
 from PySide.QtGui import *
 
 
-class sliderdemo(QWidget):
-    valueChanged = Signal(int)
+class SliderPreferences(QWidget):
+    """ represents gui interface for superformula """
+
+    sliderChanged = Signal()
 
     def __init__(self, parent=None):
-        super(sliderdemo, self).__init__(parent)
+        super(SliderPreferences, self).__init__(parent)
+
+        # (name, slider_name, text_box name)
+        self.gui_objects = []
+        self.status_bar = QStatusBar()
 
         self.grid = QGridLayout()
         names = ['A', 'B', 'M', 'N1', 'N2', 'N3']
+
         # (x, y, ) position in grid
         pos = [(0, 0), (0, 1), (0, 2),
                (1, 0), (1, 1), (1, 2),
@@ -20,13 +27,14 @@ class sliderdemo(QWidget):
                (5, 0), (5, 1), (5, 2),
                ]
         for x, y in [item for item in pos if item[1] == 0]:
-            self.lbl = QLabel(names[x])
-            self.slider = QSlider(Qt.Horizontal)
-            self.slider.setObjectName("slider" + names[x])
-            # self.slider.valueChanged.
             self.textbox = QTextEdit()
             self.textbox.setMaximumHeight(24)
             self.textbox.setObjectName("textbox" + names[x])
+            self.lbl = QLabel(names[x])
+            self.slider = QSlider(Qt.Horizontal)
+            self.slider.setObjectName("slider" + names[x])
+            self.slider.valueChanged.connect(self.slider_changed)
+            self.gui_objects.append((names[x], self.slider, self.textbox))
             self.grid.addWidget(self.lbl, x, y)
             self.grid.addWidget(self.slider, x, y + 1)
             self.grid.addWidget(self.textbox, x, y + 2)
@@ -38,14 +46,22 @@ class sliderdemo(QWidget):
         self.setLayout(self.grid)
         self.setWindowTitle("SpinBox demo")
 
-    def valuechange(self):
-        size = self.sl.value()
-        self.l1.setFont(QFont("Arial", size))
+    def slider_changed(self, arg):
+        sender = self.sender()
+        value = sender.value()
+        for name, slider, textbox in self.gui_objects:
+            if self.__eq__(sender, slider):
+                textbox.setText(str(value))
+
+    def __eq__(self, other1, other2):
+        if type(other1) is type(other2):
+            return other2.__dict__ == other1.__dict__
+        return False
 
 
 def main():
     app = QApplication(sys.argv)
-    ex = sliderdemo()
+    ex = SliderPreferences()
     ex.show()
     sys.exit(app.exec_())
 
